c912fea21081d9065b7467b51fd14fc6c56bbc1b
Adeniji Adeola Esther <adeolaesther@live.com>
2019-10-03 15:08:39 -0400

    Included the method that reads the already Existing File , the method that checks the file and extract the  data into a format useable by Java

---
+++ src/main/java/ReadFile.java
@@ -0,0 +1,119 @@
+import MapEditorModel.ContinentModel;
+import MapEditorModel.CountryModel;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadFile {
+
+
+    private  static final String CONTINENTS_TAG= "[continents]";
+    private static final String CONTINENT=" continent";
+    private static final String COUNTRY_TAG= "[countries]";
+    private static final String COUNTRY= "country";
+
+
+    private ArrayList<ContinentModel> continents = new ArrayList<ContinentModel>();
+    private ArrayList<CountryModel> countries = new ArrayList<CountryModel>();
+
+    private final String fileDirectory;
+
+    /**
+     *  This class read the name of the mapFile (File Directory) to be loaded.
+     *
+     * @param fileDirectory
+     */
+    public ReadFile (String fileDirectory) {
+        this. fileDirectory = fileDirectory;
+    }
+
+    /**
+     * This class  checks the details of the read file into an array  and stores them correctly based on countries and continents.
+     *
+     * @throws IOException
+     */
+    public void checkFile() throws IOException {
+        FileReader file = new FileReader(fileDirectory);
+        BufferedReader br = new BufferedReader(file);
+        String line = br.readLine();
+        String[] splitLine = line.split(" ");
+        String checker = " ";
+        while (line != null) {
+            line = br.readLine();
+            if(line != null){
+                if (line.equals(CONTINENTS_TAG)) {
+                    checker = CONTINENT;
+                    continue;
+                } else if (line.equals(COUNTRY_TAG) ){
+                    checker = COUNTRY;
+                    continue;
+                }
+                }
+
+                line.trim();
+            int size= line.length();
+            switch (checker) {
+                case CONTINENT:
+
+
+                    if (size >0){
+                        String[] continentData = line.split(" ");
+                        ContinentModel c1 = new ContinentModel(continentData[0], Integer.parseInt(continentData[1]));
+                        continents.add(c1);}
+                    else{
+                        continue;
+                    }
+
+                    break;
+
+
+                case COUNTRY:
+
+                    if ( size>0){
+                        String [] countryData= line.split(" ");
+                        CountryModel country = new CountryModel( Integer.parseInt( countryData[0] ), countryData[1],countryData[2]);
+                        //  Country c2= new Country(Integer.parseInt(countryData[0]),)
+                        countries.add(country);}
+                    else{
+                        continue;
+                    }
+
+                    break;
+
+            }
+
+        }
+
+
+        file.close();
+    }
+
+    /**
+     *  This classes return an arraylist containing the list of continents.
+     * @return
+     */
+
+    public ArrayList<ContinentModel> getContinents() {
+        return continents;
+    }
+
+    /**
+     *  This class returns an arraylist containing the list of country
+     * @return  Arraylists of continents and countries repectively.
+     */
+
+    public ArrayList<CountryModel> getCountries() {
+        return countries;
+    }
+
+
+
+
+}
+
+
+
+
+
