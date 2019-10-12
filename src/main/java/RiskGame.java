c599dae132ad9d88a9f1c3e42b146f6e80672db0
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-10-12 17:49:43 -0400

    Update RiskGame.java

--- src/main/java/RiskGame.java
+++ src/main/java/RiskGame.java
@@ -4,19 +4,34 @@ import java.util.Scanner;
 
 public class RiskGame {
 
-    //static String phase="MapEditor";
-    static String phase="GamePlay";
+    static String phase = "MapEditor";
+
     public static void main(String[] args) {
+        //uncomment those hardcode for testing phase
+        args[0] = "start";
+        args[1] = "new";
+        args[2] = "game";
+
+        if (args[0].equals("start") && args[1].equals("new") && args[2].equals("game")) {
+            newGame();
+        }
+    }
+
+    private static void newGame() {
 
         MapController mapController = new MapController();
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter command");
-        while(input.hasNext()){
-            String command= input.nextLine();
-            if(phase.equals("MapEditor")){
+        while (input.hasNext()) {
+            String command = input.nextLine();
+            if (command.equals("start game")) {
+                phase = "GamePlay";
+            }
+            if (phase.equals("MapEditor")) {
                 mapController.commandHandler(command.split(" "));
-            }else {
+            } else {
+                //gameController.commandHandler(command.split(" ")
                 System.out.println("GAME PLAY PHASE");
             }
         }
