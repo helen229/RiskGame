d2847429737abd22d73f0f49aa15502a15ff05e4
gao han <gaohan9409@gmail.com>
2019-10-15 20:11:40 -0400

    add ArmyModel

--- src/main/java/GamePlayModel/ArmyModel.java
+++ src/main/java/GamePlayModel/ArmyModel.java
@@ -1,4 +1,51 @@
 package GamePlayModel;
 
+import MapEditorModel.CountryModel;
+
 public class ArmyModel {
+    private PlayerModel player;
+
+    private CountryModel country;
+
+    /**
+     * Creates a new army.
+     */
+    public ArmyModel(PlayerModel player) {
+
+        this.player = player;
+    }
+
+    /**
+     * Gets the player.
+     */
+    public PlayerModel getPlayer() {
+        return player;
+    }
+
+    /**
+     * Sets the player.
+     */
+    public void setPlayer(PlayerModel player) {
+        this.player = player;
+    }
+
+    /**
+     * Gets the country.
+     */
+    public CountryModel getCountry() {
+        return country;
+    }
+
+    /**
+     * Sets the country.
+     */
+    public void setCountry(CountryModel country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        String info = "Army : player - " + player + ", country - " + country + "\n";
+        return info;
+    }
 }
