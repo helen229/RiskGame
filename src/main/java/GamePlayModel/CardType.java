946b66cfa6448088750558d9e7d7f7346e67fe28
Han Gao <ga_h@encs.concordia.ca>
2019-10-25 16:55:09 -0400

    Added the Card and CardType

---
+++ src/main/java/GamePlayModel/CardType.java
@@ -0,0 +1,48 @@
+package GamePlayModel;
+
+/**
+ * This is the class used to define the card type
+ *
+ */
+public enum CardType {
+
+    /**
+     * The infantry.
+     */
+    Infantry(0),
+
+    /**
+     * The cavalry.
+     */
+    Cavalry(1),
+
+    /**
+     * The artillery.
+     */
+    Artillery(2);
+
+    /**
+     * The card type number.
+     */
+    private int CardTypeNumber;
+
+    /**
+     * Instantiates a new card type.
+     *
+     * @param cardTypeNumber the card type number.
+     */
+    private CardType(int cardTypeNumber) {
+        this.CardTypeNumber = cardTypeNumber;
+    }
+
+    /**
+     * Gets the card type number.
+     *
+     * @return the card type number.
+     */
+    public int getCardTypeNumber() {
+        return CardTypeNumber;
+
+    }
+
+}
