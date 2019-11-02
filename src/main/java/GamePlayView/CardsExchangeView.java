79056cba4f28d26e4612b4e3dbf822f6bc53007b
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-11-02 00:06:48 -0400

    Revert "(CardExchangeView + CardExchange )correction"
    This reverts commit b4a00c658a521e3e1331df64b4d594f277909cb0.

--- src/main/java/GamePlayView/CardsExchangeView.java
+++ src/main/java/GamePlayView/CardsExchangeView.java
@@ -12,22 +12,12 @@ public class CardsExchangeView implements Observer {
     public void update(Observable obs, Object arg) {
         if (!"CardsView".equals(arg))
             return;
-        try {
-            String player = ((GameModel)obs).getCurrentPlayer().getPlayerName();
-            int currentExchangeTry = ((GameModel)obs).getCurrentExchangeTry();
-            ArrayList<Card> cardList= ((GameModel)obs).getCurrentPlayer().getCardList();
-            System.out.println("\n*************** Card Exchange View *****************\n");
-            System.out.println("Current Player is: "+player);
-            System.out.println("Currently, each card can reinforce "+ (currentExchangeTry*5)+" number of players.");
-            for (int i = 0; i < cardList.size(); i++) {
-                Card card = cardList.get(i);
-                System.out.println("Card "+i+" = "+card.getCardTypeName());
-            }
-            System.out.println("\n*************** Card Exchange View *****************\n");
-        }catch (NullPointerException e){
-            System.out.println("None Player Added yet.");
+        ArrayList<Card> cardList= ((GameModel)obs).getCurrentPlayer().getCardList();
+        for (int i = 0; i < cardList.size(); i++) {
+            Card card = cardList.get(i);
+            System.out.println("Card "+i+" "+card.getCardTypeName());
         }
-        
+
     };
 
 }
