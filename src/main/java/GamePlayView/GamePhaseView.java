24bfce2a33c10b3f6b247890b787e26eb75e5cd2
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-11-02 01:33:59 -0400

    PhaseView printing correction

--- src/main/java/GamePlayView/GamePhaseView.java
+++ src/main/java/GamePlayView/GamePhaseView.java
@@ -17,16 +17,33 @@ public class GamePhaseView implements Observer {
             //No player added yet
         }
 
-        String mapeditor = "The actions of this phase are: \nLoad map file \nEdit the game player " +
-                "\nAssign the countries \nPlace the armies\n*************** PhaseView *******************\n";
-        String startup = "The actions of this phase are: \nLoad map file \nEdit the game player " +
-                "\nAssign the countries \nPlace the armies\n*************** PhaseView *******************\n";
-        String reinforcement = "The actions of this phase are: \nReinforce the country \nExchange the cards" +
-                "\n*************** PhaseView *******************\n";
-        String attack = "The actions of this phase are: \nLoad map file \nEdit the game player " +
-                "\nAssign the countries \nPlace the armies\n*************** PhaseView *******************\n";
-        String fortification = "The actions of this phase are: \nLoad map file \nEdit the game player " +
-                "\nAssign the countries \nPlace the armies\n*************** PhaseView *******************\n";
+        String mapeditor = "The actions of this phase are: \n"
+                + "Edit the continent (editcontinent -add continentname continentvalue -remove continentname)\n"
+                + "Edit the country (editcountry -add countryname continentname -remove countryname)\n"
+                + "Edit the neighbor (editneighbor -add countryname neighborcountryname -remove countryname neighborcountryname)\n"
+                + "Show the current map (showmap) \n"
+                + "Save the map to file (savemap filename) \n"
+                + "Edit the map from file (editmap filename) \n"
+                + "Validate the current map (validatemap) \n"
+                + "*************** PhaseView *******************\n";
+        String startup = "The actions of this phase are: \n"
+                + "Load map file (loadmap filename)\n"
+                + "Edit the game player (gameplayer -add playername -remove playername)\n"
+                + "Assign the countries (populatecountries)\n"
+                + "Place the armies (placearmy countryname) (placeall)\n"
+                + "*************** PhaseView *******************\n";
+        String reinforcement = "The actions of this phase are: \n"
+                + "Reinforce the country (reinforce countryname num)\n"
+                + "Exchange the cards (exchangecards num num num –none)\n"
+                + "*************** PhaseView *******************\n";
+        String attack = "The actions of this phase are: \n"
+                + "Attack to a country (attack countrynamefrom countynameto numdice –allout –noattack)\n"
+                + "Defender defend his/her country (defend numdice)\n"
+                + "Move armies to the new country (attackmove num)\n"
+                + "*************** PhaseView *******************\n";
+        String fortification = "The actions of this phase are: \n"
+                + "Fortify armies (fortify fromcountry tocountry num –none)\n"
+                + "*************** PhaseView *******************\n";
 
         System.out.println("\n*************** PhaseView *******************\nCurrent Phase is: "+phase);
         System.out.println("Current Player is: "+player);
