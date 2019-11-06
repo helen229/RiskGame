a2a891359921ba68a2393c5b30ecc28a46e12c86
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-11-05 23:07:51 -0500

    CardsExchangeView added

--- src/main/java/GamePlayView/CardsExchangeView.java
+++ src/main/java/GamePlayView/CardsExchangeView.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package GamePlayView;
+import GamePlayModel.Card;
+import GamePlayModel.CardType;
+import GamePlayModel.GameModel;
 
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 /**
  *
  * @author Ehsan
  */
-public class CardsExchangeView {
-    
+public class CardsExchangeView  implements Observer {
+    public void update(Observable obs, Object arg) {
+        if (!"CardsView".equals(arg))
+            return;
+        String player = ((GameModel)obs).getCurrentPlayer().getPlayerName();
+        int currentExchangeTry = ((GameModel)obs).getCurrentExchangeTry();
+        ArrayList<Card> cardList= ((GameModel)obs).getCurrentPlayer().getCardList();
+        System.out.println("\n*************** Card Exchange View *****************");
+        System.out.println("Current Player is: "+player);
+        System.out.println("Currently, each card can reinforce "+ (currentExchangeTry*5)+" number of armies.");
+        for (int i = 0; i < cardList.size(); i++) {
+            Card card = cardList.get(i);
+            System.out.println("Card "+i+" = "+card.getCardTypeName());
+        }
+        System.out.println("*************** Card Exchange View *****************\n");
+    }
 }
