526cb2c465b1274e99fce903a72aefb890152879
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-11-19 00:18:46 -0500

    Update CountryModel.java

--- src/main/java/MapEditorModel/CountryModel.java
+++ src/main/java/MapEditorModel/CountryModel.java
@@ -29,7 +29,7 @@ public class CountryModel {
         this.countryName = countryName;
         this.continentName = continentName;
         this.neighbours = new ArrayList<Integer>();
-        this.owner = new PlayerModel("","");
+        this.owner = new PlayerModel("","human");
     }
 
     /**
