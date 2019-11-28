dfd65823bb707964197d3a09ca4ec86bde9cf392
Adeniji Adeola Esther <adeolaesther@live.com>
2019-11-28 16:13:04 -0500

    PlayerModelTest

---
+++ src/test/java/GamePlayModel/PlayerModelTest.java
@@ -0,0 +1,28 @@
+package GamePlayModel;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerModelTest {
+@Before
+
+
+    @Test
+    void checkPlayerContinents() {
+      
+    }
+
+    @Test
+    void getCardList() {
+    }
+
+    @Test
+    void percentageOfmap() {
+    }
+
+    @Test
+    void addArmyNum() {
+    }
+}
