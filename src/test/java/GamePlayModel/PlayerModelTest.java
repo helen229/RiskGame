106a133f85a6e958e46c19ee6a9c0f771fc4ef38
Adeniji Adeola Esther <adeolaesther@live.com>
2019-11-29 16:14:42 -0500

    GamePlayerModelTest and GameControllerTest

--- src/test/java/GamePlayModel/PlayerModelTest.java
+++ src/test/java/GamePlayModel/PlayerModelTest.java
@@ -1,24 +1,38 @@
 package GamePlayModel;
 
+import MapEditorModel.ContinentModel;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerModelTest {
+    @BeforeAll
+    public void setup() {
 
+
+
+
+}
     @Test
     void checkPlayerContinents() {
+        PlayerModel player = new PlayerModel("Test");
+        ArrayList<ContinentModel> ContinentList= new ArrayList<>();
+        player.checkPlayerContinents(ContinentList);
+
+        assertNull(player.playerContinents);
     }
 
-    @Test
-    void getNumReinforceArmyRemainPlace() {
-    }
 
-    @Test
-    void setNumReinforceArmyRemainPlace() {
-    }
 
     @Test
     void percentageOfmap() {
+        PlayerModel player = new PlayerModel("Test");
+        ArrayList<ContinentModel> ContinentList= new ArrayList<>();
+       int countryNum=3;
+       assertNotNull(player.percentageOfmap(countryNum));
     }
 }
