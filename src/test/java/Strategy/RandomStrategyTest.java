d2d2f3902430a509eb90c47b46f37e241b428424
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-12-04 12:20:07 -0500

    Unit tests

---
+++ src/test/java/Strategy/RandomStrategyTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strategy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+/**
+ *
+ * @author Ehsan
+ */
+public class RandomStrategyTest {
+    
+}
