3f7ec5ce799901420d209650d6b1346c06a9b6d3
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-12-04 13:34:44 -0500

    Units correction

--- src/test/java/Strategy/AggressiveStrategyTest.java
+++ src/test/java/Strategy/AggressiveStrategyTest.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package Strategy;
-
+import GamePlayModel.GameModel;
+import GamePlayModel.PlayerModel;
+import MapEditorModel.ContinentModel;
+import MapEditorModel.CountryModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +20,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Ehsan
  */
 public class AggressiveStrategyTest {
+    private ContinentModel continent1;
+    private CountryModel country1;
+    private PlayerModel player1;
+    private GameModel gameModel;
+    private AggressiveStrategy aggressiveStrategy;
     
+    /**
+     * Each time invoke a method, set up this context
+     */
+    @BeforeEach
+    public void setUp() {
+
+        player1 = new PlayerModel("Player1");
+        gameModel= new GameModel.Builder().currentExchangeTry(1).currentPlayerNum(0).build();
+        continent1= new ContinentModel ("Continent1",1);
+        country1= new CountryModel (0,"Country1","Continent1");
+        aggressiveStrategy= new AggressiveStrategy (player1, gameModel );
+        ArrayList<String> playerList = new ArrayList<String>();
+        playerList.add(player1.getPlayerName());
+    }
+    
+    @Test
+    public void testGetName() {
+        assertTrue(aggressiveStrategy.getName().equals("Aggressive"));
+    }
 }
--- src/test/java/Strategy/RandomStrategyTest.java
+++ src/test/java/Strategy/RandomStrategyTest.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package Strategy;
+import GamePlayModel.GameModel;
+import GamePlayModel.PlayerModel;
+import MapEditorModel.ContinentModel;
+import MapEditorModel.CountryModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +20,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Ehsan
  */
 public class RandomStrategyTest {
+    private ContinentModel continent1;
+    private CountryModel country1;
+    private PlayerModel player1;
+    private GameModel gameModel;
+    private RandomStrategy randomStrategy;
     
+    /**
+     * Each time invoke a method, set up this context
+     */
+    @BeforeEach
+    public void setUp() {
+
+        player1 = new PlayerModel("Player1");
+        gameModel= new GameModel.Builder().currentExchangeTry(1).currentPlayerNum(0).build();
+        continent1= new ContinentModel ("Continent1",1);
+        country1= new CountryModel (0,"Country1","Continent1");
+        randomStrategy= new RandomStrategy (player1, gameModel );
+        ArrayList<String> playerList = new ArrayList<String>();
+        playerList.add(player1.getPlayerName());
+    }
+    
+    @Test
+    public void testGetName() {
+        assertTrue(randomStrategy.getName().equals("Random"));
+    }
 }
