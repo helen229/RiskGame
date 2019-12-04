44c517cd94cee22d9999a995fbbd7ee9504343e3
Ehsan67m <ehsan.marjani@polymtl.ca>
2019-12-04 13:43:32 -0500

    Units correction

--- src/test/java/Strategy/BenevolentStrategyTest.java
+++ src/test/java/Strategy/BenevolentStrategyTest.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package Strategy;
+import GamePlayModel.GameModel;
+import GamePlayModel.PlayerModel;
+import MapEditorModel.ContinentModel;
+import MapEditorModel.CountryModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,10 +15,35 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  *
  * @author Ehsan
  */
 public class BenevolentStrategyTest {
+    private ContinentModel continent1;
+    private CountryModel country1;
+    private PlayerModel player1;
+    private GameModel gameModel;
+    private BenevolentStrategy benevolentStrategy;
     
+    /**
+     * Each time invoke a method, set up this context
+     */
+    @BeforeEach
+    public void setUp() {
+
+        player1 = new PlayerModel("Player1");
+        gameModel= new GameModel.Builder().currentExchangeTry(1).currentPlayerNum(0).build();
+        continent1= new ContinentModel ("Continent1",1);
+        country1= new CountryModel (0,"Country1","Continent1");
+        benevolentStrategy= new BenevolentStrategy (player1, gameModel );
+        ArrayList<String> playerList = new ArrayList<String>();
+        playerList.add(player1.getPlayerName());
+    }
+    
+    @Test
+    public void testGetName() {
+        assertTrue(benevolentStrategy.getName().equals("Benevolent"));
+    }
 }
